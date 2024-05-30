600ea7e7fa848e62b46aefa0e1a758fdaa6c9414
maxig099 <maximoagomez@sanluis.edu.ar>
2024-05-30 04:14:20 -0300

    carrusel

--- script.js
+++ script.js
@@ -1,15 +1,52 @@
-let siguiente = document.querySelector("sig");
-let atras = document.querySelector("atras");
-let imgActual = document.querySelector("img")
-let cont = 0;
-
-function atras() {
-    atras.addEventListener('click', e=>{
-let tgt = e.target
-if(tgt === atras) {
-imgActual.style.zIndex = -1;
+function carrusel(){
+    document.addEventListener('DOMContentLoaded', () => {
+        let siguiente = document.querySelector(".sig");
+        let atras = document.querySelector(".atras");
+        let imagenes = document.querySelectorAll(".carruselImg img");
+        let cont = 0;
+    
+        siguiente.addEventListener('click', () => {
+            imagenes[cont].classList.remove('actual');
+            cont = cont + 1;
+            if (cont >= imagenes.length) {
+                cont = 0; 
+            }
+            imagenes[cont].classList.add('actual');
+        });
+        
+    
+        atras.addEventListener('click', () => {
+            imagenes[cont].classList.remove('actual');
+            cont = cont - 1;
+            if (cont < 0) {
+                cont = imagenes.length - 1;
+            }
+            imagenes[cont].classList.add('actual');
+        });
+        
+    });
 }
-    })
-    }
 
+const expRegNom = /^[a-zA-Z]$/;
+
+function validarDatos() {
+    const nombre = document.getElementById('nombre');
+    const apellido = document.getElementById('apellido');
+    const email = document.getElementById('email');
+
+    console.log(nombre.value)
+
+    if(nombre.value == "" || !expRegNom.test(nombre.value)) {
+        console.log("Error")
+    }
+    if(apellido.value == "" || !expRegNom.test(apellido.value)) {
+        console.log("Error")
+    }
+    if(email.value == "" || !expRegNom.test(nombre.value)) {
+        console.log("Error")
+    }
+    return false;
+}
+
+carrusel();
 
