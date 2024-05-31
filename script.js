673cc2b2bcf3ec38d0d5b02adff735ab6820932b
maxig099 <maximoagomez@sanluis.edu.ar>
2024-05-31 06:03:18 -0300

    validacion de datos

--- script.js
+++ script.js
@@ -27,26 +27,64 @@ function carrusel(){
     });
 }
 
-const expRegNom = /^[a-zA-Z]$/;
+const expRegNom = /^[a-zA-Z\s]+$/;
+        const expRegMail = /^[a-zA-Z0-9._-]+@[a-zA-Z]+\.[a-zA-Z]{2,}$/;
 
-function validarDatos() {
-    const nombre = document.getElementById('nombre');
-    const apellido = document.getElementById('apellido');
-    const email = document.getElementById('email');
+        function validarDatos() {
+            const nombre = document.getElementById('nom');
+            const nom = nombre.value.trim();
+            const apellido = document.getElementById('apell');
+            const apell = apellido.value.trim();
+            const email = document.getElementById('email');
+            const correo = email.value.trim();
+            let nomVacio = document.getElementById('errorNombreVacio');
+            let nomInv = document.getElementById('errorNombreInvalido');
+            let apellVacio = document.getElementById('errorApellidoVacio');
+            let apellInv = document.getElementById('errorApellidoInvalido');
+            let emailVacio = document.getElementById('errorEmailVacio')
+            let emailInv = document.getElementById('errorEmailInvalido')
+          
 
-    console.log(nombre.value)
+            for (v of frm.querySelectorAll("input, label")){
+                    v.classList.remove("error");
+                    v.classList.remove("visible")
 
-    if(nombre.value == "" || !expRegNom.test(nombre.value)) {
-        console.log("Error")
-    }
-    if(apellido.value == "" || !expRegNom.test(apellido.value)) {
-        console.log("Error")
-    }
-    if(email.value == "" || !expRegNom.test(nombre.value)) {
-        console.log("Error")
-    }
-    return false;
-}
+            }
+
+            if (nom == "") {
+                nomVacio.classList.add('visible');
+                nombre.classList.add('error');
+               
+            } else if (!expRegNom.test(nom)) {
+                nomInv.classList.add('visible');
+                nombre.classList.add('error');
+                
+            }
+
+            if (apell == "") {
+                apellVacio.classList.add('visible');
+                apellido.classList.add('error');
+                
+            } else if (!expRegNom.test(apell)) {
+                apellInv.classList.add('visible');
+                apellido.classList.add('error');
+                
+            }
+
+            if (correo == "") {
+                emailVacio.classList.add('visible');
+                email.classList.add('error');
+                
+            } else if (!expRegMail.test(correo)) {
+                emailInv.classList.add('visible');
+                email.classList.add('error');
+                
+            }
+
+            
+
+            return false;
+        }
 
 carrusel();
 
